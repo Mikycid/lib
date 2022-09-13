d4938ba7fbc8743b3b49dbc9e9e93f5310d292d7
Mike <lepavecmike@hotmail.fr>
2022-09-14 01:04:21 +0200

    Added python-like ranges in C, soon trying multithreaded range performance, and will also add that for CPP

---
+++ C/Utils/MinMax.h
@@ -0,0 +1,23 @@
+#ifndef DEF_C_MINMAX
+#define DEF_C_MINMAX
+
+#if defined(__GNUC__) || defined(__clang__)
+#define max(a,b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a > _b ? _a : _b;       \
+})
+
+#define min(a,b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a < _b ? _a : _b;       \
+})
+#else
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#endif
+
+#endif
